27c4d5dacc83d23150c470666acdb426a06249f5
cloudcreatr <79000269+cloudcreatr@users.noreply.github.com>
2023-07-18 08:56:14 +0000

    weather bro

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,115 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import { useState, useEffect } from "react";
+import { Dialog } from "@headlessui/react";
+
+const LoadingBar = () => (
+  <div className="h-[20px] w-[100%]  max-w-4xl bg-gray-200 rounded-lg animate-pulse"></div>
+);
+
+function Loading() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex flex-col gap-3 w-full p-9">
+      <LoadingBar />
+      <LoadingBar />
+      <LoadingBar />
+      <LoadingBar />
+      <LoadingBar />
+      <LoadingBar />
+      <LoadingBar />
+      <LoadingBar />
+    </div>
+  );
+}
+
+export default function Page() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const [count, setCount] = useState(0);
+  const data = [
+    "Searching Location...",
+    "Analyzing Clouds...",
+    "Looking Outside Your Window...",
+    "Gathering Last Information...",
+  ];
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      if (isOpen) {
+        if (count === 4) {
+          setLoading(false);
+          console.log("i am running");
+          clearInterval(interval);
+        } else {
+          setCount((count) => count + 1);
+          console.log(data[count]);
+        }
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [count, isOpen]);
+
+  useEffect(() => {
+    if (!isOpen) setCount(0);
+  }, [isOpen]);
+
+  return (
+    <div className="m-auto mx-0">
+      <div className="flex flex-col justify-center items-center h-screen gap-8">
+        <h1 className="text-xl font-bold">Enter Your Location</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setIsOpen(true);
+          }}
+          className="flex flex-col gap-4 w-full px-4 max-w-md"
+        >
+          <input
+            type="text"
+            className="bg-gray-100 rounded-md p-3 focus:outline-black focus:outline-offset-4 focus:outline-2"
+          />
+
+          <button
+            className="bg-slate-900 text-white rounded-md p-3 font-semibold "
+            type="submit"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            Check Weather
+          </button>
+        </form>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+        <div
+          className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm"
+          aria-hidden="true"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="fixed inset-0 flex items-center justify-center p-6">
+          <Dialog.Panel className="bg-white h-[80vh] w-full rounded-lg max-w-2xl border-2 border-gray-200 flex flex-col justify-center items-center p-4">
+            {loading ? (
+              <>
+                <Dialog.Title className="text-2xl font-semibold text-center">
+                  {data[count]}
+                </Dialog.Title>
+                <Loading />
+              </>
+            ) : (
+              <div className="flex flex-col items-center justify-center gap-3">
+                <div className="text-2xl font-semibold text-center">
+                  idk, just look outside.
+                </div>
+                <button
+                  className="bg-slate-900 text-white rounded-md p-3 font-semibold "
+                  onClick={() => setIsOpen(false)}
+                >
+                  Close
+                </button>
+              </div>
+            )}
+          </Dialog.Panel>
+        </div>
+      </Dialog>
+    </div>
+  );
 }
